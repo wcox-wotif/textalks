c160f7bdb6964239ca5beadd6a051dbe3f602594
wcox <warwick.cox@wotifgroup.com>
2015-04-08 14:37:15 +1000

    update bd file to live one

--- php/db.php
+++ php/db.php
@@ -1,39 +1,15 @@
 <?php
 /// Local Host
-function dbConnect($type) {
-	if ($type == 'admin') {
-		$user = 'TEX';
-		$pwd = 'R8rLdyDB9566J7hX';
-	} else {
-		exit('Unregognisable connection type');
-	}
-	// Connection code goes here
-	$host='localhost';
-	$database='TEX';
-	$dsn="mysql:host=$host;dbname=$database";
-	try {
-		$conn = new PDO($dsn,$user,$pwd);
-		return $conn;
-	} 
-	catch (PDOException $e) {
-		echo 'Cannot connect to database';
-		exit;
-	}
-}
-
-
-
- // Live Website
 // function dbConnect($type) {
 // 	if ($type == 'admin') {
-// 		$user = 'qwebdevc_tex';
-// 		$pwd = 'lL9FWL424pTH';
+// 		$user = 'TEX';
+// 		$pwd = 'R8rLdyDB9566J7hX';
 // 	} else {
 // 		exit('Unregognisable connection type');
 // 	}
 // 	// Connection code goes here
 // 	$host='localhost';
-// 	$database='qwebdevc_textalks';
+// 	$database='TEX';
 // 	$dsn="mysql:host=$host;dbname=$database";
 // 	try {
 // 		$conn = new PDO($dsn,$user,$pwd);
@@ -44,6 +20,30 @@ function dbConnect($type) {
 // 		exit;
 // 	}
 // }
+
+
+
+ // Live Website
+function dbConnect($type) {
+	if ($type == 'admin') {
+		$user = 'qwebdevc_tex';
+		$pwd = 'lL9FWL424pTH';
+	} else {
+		exit('Unregognisable connection type');
+	}
+	// Connection code goes here
+	$host='localhost';
+	$database='qwebdevc_textalks';
+	$dsn="mysql:host=$host;dbname=$database";
+	try {
+		$conn = new PDO($dsn,$user,$pwd);
+		return $conn;
+	} 
+	catch (PDOException $e) {
+		echo 'Cannot connect to database';
+		exit;
+	}
+}
 
 
 ?>
