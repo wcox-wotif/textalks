c99ca9b711586428eb9ffacec6824ef41de7ead8
Warwick Cox <wcox@BNEC02PPP0XG8WN.sea.corp.expecn.com>
2015-06-19 15:11:40 +1000

    added Google Analytics to the site

--- index.php
+++ index.php
@@ -16,6 +16,16 @@
     <title><?php print $pageInfo['page_title'] ?></title>
 </head>
 <body class="<?php print $pageInfo['page_name']; ?>">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-64250082-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/includes/header.php'; ?>
 
 <?php 
